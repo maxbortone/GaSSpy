3b9ca87fdb8a58831b4c6884badb94ce28514371
Max Bortone <maxbortone@gmail.com>
2017-05-11 15:08:45 +0200

    refactoring and clean up

--- lib/stack.py
+++ lib/stack.py
@@ -14,7 +14,8 @@ from matplotlib.ticker import MultipleLocator
 mpl.rcParams['text.usetex'] = True
 mpl.rcParams['text.latex.preamble'] = [
        r'\usepackage{siunitx}',
-       r'\DeclareSIUnit\ergs{ergs}'
+       r'\DeclareSIUnit\ergs{ergs}',
+       r'\sisetup{per-mode=symbol}'
 ]
 
 def gaussian(x, amp, cen, wid):
@@ -42,13 +43,15 @@ class Stack:
         else:
             self.spectra = None
             self.N = 0
-        if name:
-            self.name = name
         # initialize attributes
+        # name of the stack
+        self.name = None
         # wheter sky lines are being masked or not
         self.masking = None
         # length of stacked spectrum
         self.M = None
+        # weights by which each spectra is multiplied in the stacking process
+        self.weights = np.empty(self.N)
         # stores the weighted sum of the spectra at each wavelength pixel
         self.P = None
         # stores the sum of the weights of the spectra at each wavelength pixel
@@ -59,14 +62,14 @@ class Stack:
         # TODO: define a working flux that can be set to the stacked flux
         #  or the bias corrected one
         self.flux = None
-        # noise of stacked spectrum
-        self.noise = None
+        # flux error of stacked spectrum
+        self.error = None
         # number of spectra that contributed to each wavelength pixel
         self.contributions = None
         # dispersion of the sample at each wavelength pixel
         self.dispersion = None
         # signal to noise ratio of the stacked spectrum
-        self.SN = None
+        self.SNR = None
         # MILES template that is being used for the ppxf fit
         self.template = None
         # ppxf fit result
@@ -75,7 +78,7 @@ class Stack:
         self.residual = None
         # weights by which each template was multiplied to best fit
         # the stacked spectrum
-        self.weights = None
+        self.temp_weights = None
         # mean logarithmic age
         self.mean_log_age = None
         # mean metallicity
@@ -85,23 +88,19 @@ class Stack:
         self.mass_to_light = None
 
     """
-    Calculate the weighted average of the stack
+    Prepare the spectra for stacking: deredshift, normalize, compute SNR and
+    interpolate flux and error
 
     INPUT:
         wr:     tuple or array of two floats representing a wavelength range,
-                e.g. [4100, 4700]
-        gs:     spacing of wavelength grid, e.g. 1 angstrom
+                e.g. [4100, 4700], to which the spectra are normalize
+        wlr:    list or array of wavelength regions where to compute the SNR
+        gs:     spacing of wavelength grid, e.g. 1 angstrom, for the interpolation
         wl:     wavelengths of the sky spectrum to be masked
         dw:     width of region around wl to be masked
-        dc:     wheter to use th edust corrected flux or not
-
-    OUTPUT:
-        lam:    wavelenghts of stacked spectrum
-        flux:   flux of stacked spectrum
-        noise:  noise of stacked spectrum
-        contrs: contributions from spectra to flux at each wavelength
+        dc:     wheter to use the dust corrected flux or not
     """
-    def average(self, wr, gs, wl=None, dw=None, dc=False):
+    def prepare_spectra(self, wr, wlr, gs, wl=None, dw=None, dc=False):
         # set masking flag
         self.masking = (wl is not None) and (dw is not None)
         # prepare spectra
@@ -109,9 +108,43 @@ class Stack:
             sp = self.spectra[i]
             sp.deredshift()
             sp.normalize(wr, dc)
+            sp.signaltonoise(wlr)
             sp.interpolate(gs)
             if self.masking:
                 sp.setmask(wl, dw)
+
+    """
+    Determine the weights for eah spectra in the stack from their SNR
+    """
+    def determine_weights(self):
+        for i in range(self.N):
+            sp = self.spectra[i]
+            self.weights[i] = sp.SNR
+        # normalize weights
+        self.weights = self.weights / sum(self.weights)
+
+    """
+    Calculate the weighted average of the stack
+
+    NOTE: run prepare_spectra and determine_weights before average!
+
+    OUTPUT:
+        lam:    wavelenghts of stacked spectrum
+        flux:   flux of stacked spectrum
+        error:  flux error of stacked spectrum
+        contrs: contributions from spectra to flux at each wavelength
+    """
+    def average(self):
+        # # set masking flag
+        # self.masking = (wl is not None) and (dw is not None)
+        # # prepare spectra
+        # for i in range(self.N):
+        #     sp = self.spectra[i]
+        #     sp.deredshift()
+        #     sp.normalize(wr, dc)
+        #     sp.interpolate(gs)
+        #     if self.masking:
+        #         sp.setmask(wl, dw)
         # find maximal common wavelength range limits
         w1 = self.spectra[0].lam_interp[0]
         w2 = self.spectra[0].lam_interp[-1]
@@ -130,7 +163,7 @@ class Stack:
         self.M = int(w2-w1+1)
         lam = self.spectra[0].lam_interp
         flux = np.empty(self.M)
-        noise = np.empty(self.M)
+        error = np.empty(self.M)
         contrs = np.empty(self.M)
         P = np.empty(self.M)
         S = np.empty(self.M)
@@ -146,30 +179,30 @@ class Stack:
                 if self.masking and sp.mask[i]:
                     c -= 1
                 else:
-                    p += sp.flux_interp[i] * sp.noise_interp[i]
-                    s += sp.noise_interp[i]
-                    n += sp.noise_interp[i]**2
+                    p += sp.flux_interp[i] * self.weights[j]
+                    s += self.weights[j]
+                    n += sp.error_interp[i]**2 * self.weights[j]**2
             if s == 0:
                 flux[i] = 0
             else:
                 P[i] = p
                 S[i] = s
                 flux[i] = p / s
-            noise[i] = np.sqrt(n) / c
+            error[i] = np.sqrt(n)
             contrs[i] = c
         self.wave = lam
         self.flux = flux
-        self.noise = noise
+        self.error = error
         self.contributions = contrs
         self.P = P
         self.S = S
-        return lam, flux, noise, contrs
+        return lam, flux, error, contrs
 
     """
     Use the jackknife method to estimate the dispersion in the sample
     """
     def jackknife(self):
-        # TODO: assert flux_interp and noise_interp are not None
+        # TODO: assert flux_interp and error_interp are not None
         disp = np.empty(self.M)
         for i in range(self.M):
             p = self.P[i]
@@ -178,9 +211,9 @@ class Stack:
             for j in range(self.N):
                 sp = self.spectra[j]
                 if not (self.masking and sp.mask[i]):
-                    n = sp.noise_interp[i]
+                    w = self.weights[j]
                     f = sp.flux_interp[i]
-                    g += ((p*(s-n) - s*(p-n*f)) / (s*(s-n)))**2
+                    g += ((p*(s-w) - s*(p-w*f)) / (s*(s-w)))**2
             c = self.contributions[i]
             disp[i] = g * (c - 1) / c
         self.dispersion = np.sqrt(disp)
@@ -190,47 +223,32 @@ class Stack:
             #   - continuum at 6800-7200
             # give out percentage difference
             if self.name:
-                print("Sample dispersion of {} is high".format(self.name))
+                print("Warning: sample dispersion of {} is high".format(self.name))
             else:
-                print("Sample dispersion is high")
-        return disp
-
-    """
-    Correct the stacked spectrum for possible sampling bias
-    """
-    def correct(self):
-        # TODO: assert flux, flux_interp and noise_interp are not None
-        corr = np.zeros(self.M)
-        for i in range(self.M):
-            p = self.P[i]
-            s = self.S[i]
-            c = self.contributions[i]
-            for j in range(self.N):
-                sp = self.spectra[j]
-                if not (self.masking and sp.mask[i]):
-                    n = sp.noise_interp[i]
-                    f = sp.flux_interp[i]
-                    # TODO: check why s = n
-                    # seems to be happen when c = 1
-                    if s != n:
-                        corr[i] += (p - n*f) / (s - n)
-            corr[i] = (c-1)*(corr[i]/c - self.flux[i])
-        self.correction = corr
-        self.flux -= self.correction
-        return corr
+                print("Warning: sample dispersion is high")
 
     """
     Computes the signal-to-noise ratio of the stacked spectrum
 
     INPUT:
-        wr: tuple or array of two floats representing a wavelength range,
-            e.g. [4100, 4700]
+        wlr: list or array of wavelength regions where to compute the SNR
+        flag: boolean, if True outputs SNR value in each wavelength region
     """
-    def signaltonoise(self, wr):
-        idx = (self.wave >= wr[0]) & (self.wave <= wr[1])
-        S = self.flux[idx].mean()
-        N = self.noise[idx].mean()
-        self.SN = S / N
+    def signaltonoise(self, wlr, flag=False):
+        m = wlr.shape[0]
+        SNRs = np.empty(m)
+        S = np.empty(m)
+        N = np.empty(m)
+        for i in range(m):
+            idx = (self.wave >= wlr[i][0]) & (self.wave <= wlr[i][1])
+            s = np.median(self.flux[idx])
+            n = np.median(self.error[idx])
+            SNRs[i] = s / n
+            S[i] = s
+            N[i] = n
+        self.SNR = np.median(S) / np.median(N)
+        if flag:
+            return SNRs
 
     """
     Fit the stacked spectrum using the Penalized Pixel Fitting method ppxf by
@@ -283,15 +301,15 @@ class Stack:
             loglam = np.log(wave)
             # select noise
             mask = (self.wave > 3540) & (self.wave < 7409)
-            noise = self.noise[mask]
+            noise = self.error[mask]
         else:
             # run bias correction
-            self.correct()
+            self._correct()
             # bring galaxy spectrum to same wavelength range as stellar library
             mask = (self.wave > 3540) & (self.wave < 7409)
             wave = self.wave[mask]
             flux = self.flux[mask]
-            noise = self.noise[mask]
+            noise = self.error[mask]
             # rebin flux logarithmically for ppxf
             flux, loglam, velscale = util.log_rebin([wave[0], wave[-1]], flux)
             flux = flux / np.median(flux)
@@ -332,12 +350,12 @@ class Stack:
                   goodpixels=goodpixels, plot=False, moments=4, degree=-1,
                   vsyst=dv, clean=False, mdegree=10, regul=1./delta,
                   quiet=True)
-        weights = pp.weights.reshape(miles.n_ages, miles.n_metal)/pp.weights.sum()
-        self.mean_log_age, self.mean_metal = miles.mean_age_metal(weights, quiet=True)
-        self.mass_to_light = miles.mass_to_light(weights, band="r", quiet=True)
+        temp_weights = pp.weights.reshape(miles.n_ages, miles.n_metal)/pp.weights.sum()
+        self.mean_log_age, self.mean_metal = miles.mean_age_metal(temp_weights, quiet=True)
+        self.mass_to_light = miles.mass_to_light(temp_weights, band="r", quiet=True)
         self.pp = pp
         self.pp.lam = np.exp(loglam)
-        self.weights = weights
+        self.temp_weights = temp_weights
         self.residual = self.pp.galaxy - self.pp.bestfit
         if refit:
             self.gaussians = gaussians
@@ -351,19 +369,25 @@ class Stack:
     """
     def plotStack(self, filename=None, title=None):
         wv = self.wave
-        f, axes = plt.subplots(2, 1, sharex=True, figsize=(16, 9))
+        f, axes = plt.subplots(3, 1, sharex=True, figsize=(16, 9))
+        if self.correction is not None:
+            axes[0].text(0.75, 0.85, "bias correction: ON", bbox=dict(facecolor='red', alpha=0.5), transform=axes[0].transAxes)
+        else:
+            axes[0].text(0.75, 0.85, "bias correction: OFF", bbox=dict(facecolor='red', alpha=0.5), transform=axes[0].transAxes)
         axes[0].plot(wv, self.flux, 'b', label="stacked flux")
-        # axes[0].fill_between(wv, fl-ns, fl+ns, facecolor="blue", alpha="0.5")
-        axes[0].plot(wv, self.noise, 'g', label="noise")
-        axes[0].plot(wv, self.dispersion, 'r', label="dispersion")
-        axes[0].plot(wv, self.flux-self.correction, 'y', label="bias corrected")
+        axes[0].plot(wv, self.error, 'g', label="error")
         axes[0].set_ylabel(r"flux [\SI{e-17}{\ergs\per\second\per\square\centi\meter\per\angstrom}]")
-        axes[0].legend(loc="best", frameon=False)
-        axes[1].plot(wv, self.contributions)
-        axes[1].set_ylabel("number of spectra")
-        axes[1].set_xlabel(r"wavelength [\si{\angstrom}]")
-        axes[1].set_ylim([self.contributions.min()-5, self.N+5])
-        axes[1].yaxis.set_major_locator(MultipleLocator(5))
+        axes[0].legend(loc="upper left", frameon=False)
+        axes[1].plot(wv, self.dispersion, 'r', label="dispersion")
+        axes[1].plot(wv, self.correction, 'y', label="bias correction")
+        axes[1].set_ylabel(r"flux [\SI{e-17}{\ergs\per\second\per\square\centi\meter\per\angstrom}]")
+        axes[1].legend(loc="upper left", frameon=False)
+        axes[2].plot(wv, self.contributions, label="contributions")
+        axes[2].set_ylabel("number of spectra")
+        axes[2].set_xlabel(r"wavelength [\si{\angstrom}]")
+        axes[2].set_ylim([self.contributions.min()-5, self.N+5])
+        axes[2].yaxis.set_major_locator(MultipleLocator(5))
+        axes[2].legend(loc="lower left", frameon=False)
         if title is not None:
             axes[0].set_title(title)
         if filename:
@@ -384,6 +408,8 @@ class Stack:
         axes.plot(wv, self.pp.galaxy, 'b', label="stacked spectrum")
         axes.plot(wv, self.pp.bestfit, 'g', label="ppxf fit")
         axes.plot(wv, self.residual, 'r', label="residual")
+        if self.SNR:
+            axes.text(0.75, 0.05, "SNR = {:.2f}".format(self.SNR), bbox=dict(facecolor='red', alpha=0.5), transform=axes.transAxes)
         axes.set_ylabel(r"flux [\SI{e-17}{\ergs\per\second\per\square\centi\meter\per\angstrom}]")
         axes.set_xlabel(r"wavelength [\si{\angstrom}]")
         axes.legend(loc="best", frameon=False)
@@ -444,6 +470,26 @@ class Stack:
     ###########################################################################
     # PRIVATE METHODS
     ###########################################################################
+    """
+    Correct the stacked spectrum for possible sampling bias
+    """
+    def _correct(self):
+        corr = np.zeros(self.M)
+        for i in range(self.M):
+            p = self.P[i]
+            s = self.S[i]
+            c = self.contributions[i]
+            for j in range(self.N):
+                sp = self.spectra[j]
+                if not (self.masking and sp.mask[i]):
+                    w = self.weights[j]
+                    f = sp.flux_interp[i]
+                    if s != w:
+                        corr[i] += (p - w*f) / (s - w)
+            corr[i] = (c-1)*(corr[i]/c - self.flux[i])
+        self.correction = corr
+        self.flux -= self.correction
+
     """
     Detect the presence of a peak at @line using sigma clipping. A peak is
     detected if a positive 3sigma outlier is found within 3 angstrom from @line
@@ -541,24 +587,46 @@ if __name__ == "__main__":
     spectra_files = [os.path.join(spectra_path, f) for f in os.listdir(spectra_path) if os.path.isfile(os.path.join(spectra_path, f))]
     t = clock()
     stack = Stack(spectra_files)
-    print("- stack initialization: {}".format(clock()-t))
+    print("- stack initialization: {}s".format(clock()-t))
 
     # compute stacked spectrum with masking and dust correction
     wr = [4100, 4700]                   # wavelength range for normalization
+    wlr = np.array([[4500, 4700],       # wavelength regions for SNR analysis
+                    [5400, 5600],
+                    [6000, 6200],
+                    [6800, 7000]])
     gs = 1.0                            # grid spacing for interpolation
     wl = np.array([5577, 6300, 6363])   # sky lines for masking
     dw = 3                              # broadness of sky lines for masking
     dc = True                           # flag for dust correction
     tp = 'kb'                           # Kroupa IMF
 
+    # prepare spectra for stacking
     t = clock()
-    stack.average(wr, gs, wl=wl, dw=dw, dc=dc)
-    print("- stacking: {}".format(clock()-t))
+    stack.prepare_spectra(wr, wlr, gs, wl=wl, dw=dw, dc=dc)
+    print("- prepare spectra: {}s".format(clock()-t))
+    # determine weights from SNR
+    t = clock()
+    stack.determine_weights()
+    print("- determine weights: {}s".format(clock()-t))
+    # stack
+    t = clock()
+    stack.average()
+    print("- stacking: {}s".format(clock()-t))
+
+    # compute dispersion of the spectra in the stack using the jackknife method
+    t = clock()
+    stack.jackknife()
+    print("- dispersion: {}s".format(clock()-t))
 
     # fit stacked spectrum with ppxf
     t = clock()
     stack.ppxffit(temp=tp)
-    print("- ppxf fit: {}".format(clock()-t))
+    print("- ppxf fit: {}s".format(clock()-t))
+
+    # compute SNR of stacked spectrum
+    stack.signaltonoise(wlr)
 
     # plot results
+    stack.plotStack()
     stack.plotFit()
